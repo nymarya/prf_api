020ce32c49bc70a76703b50ecdc2502ecb0c45a3
Mayra Azevedo <mayradazevedo@gmail.com>
2019-08-01 21:21:11 -0300

    adiciona exceções

--- brazilian_roads_api/RoadsApi.py
+++ brazilian_roads_api/RoadsApi.py
@@ -13,9 +13,23 @@ class RoadsApi:
 
         self._carregar_links_infracoes()
 
+    def _exibir_erro(self, ex: Exception, msg: str):
+        """Exibir mensagem personalizada para erro"""
+        print('\033[91m{}\033[0m'.format(msg))
+        print(ex)
+
     def _carregar_links_infracoes(self):
         """Função que busca o link para cada infração"""
-        response = requests.get(self.url)
+        try:
+            response = requests.get(self.url)
+        except requests.exceptions.ConnectionError as ex:
+            self._exibir_erro(ex, "Falha de conexão. "
+                              "Verifique sua conexão e tente novamente.")
+            return
+        except Exception as ex:
+            self._exibir_erro(ex, "Ocorreu um erro inesperado. "
+                              "Verifique sua conexão e tente novamente.")
+            return
 
         # Recupera o HTML
         html_data = response.text
@@ -25,5 +39,5 @@ class RoadsApi:
         links = soup.findAll("a", {"class": "internal-link"})
 
         # Filtra links referentes aos anos
-        self.links_infracoes = {link.text: link['href'] for link in links
-                                if link.text.startswith('20')}
+        self.links_infracoes = {link.text: link['href'].split('/')[-1]
+                                for link in links if link.text.startswith('20')}
